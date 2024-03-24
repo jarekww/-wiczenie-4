d9d418c5f6ae8aabe07f3ee63c68e1b90eb395e6
jarekww <93822629+jarekww@users.noreply.github.com>
2024-03-24 09:44:04 +0100

    Dodaj pliki projektów.

---
+++ ćwiczenie 4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+interface IShape
+{
+    double GetArea();
+}
+
+
+class SquareAdapter : IShape
+{
+    private Square square;
+
+    public SquareAdapter(Square square)
+    {
+        this.square = square;
+    }
+
+    public double GetArea()
+    {
+        return square.CalculateArea();
+    }
+}
+
+
+class Square
+{
+    private double sideLength;
+
+    public Square(double sideLength)
+    {
+        this.sideLength = sideLength;
+    }
+
+    public double CalculateArea()
+    {
+        return sideLength * sideLength;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Square square = new Square(5);
+        SquareAdapter adapter = new SquareAdapter(square);
+        UseShape(adapter);
+    }
+
+    static void UseShape(IShape shape)
+    {
+        double area = shape.GetArea();
+        Console.WriteLine($"Area: {area}");
+    }
+}
